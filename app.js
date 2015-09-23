e278528d14334401353e5fea3371898cb890557e
Jason <quizathaderat@yahoo.com>
2015-09-22 21:20:43 -0700

    Add button to show form

--- app.js
+++ app.js
@@ -22,7 +22,22 @@ app.post('/rentals.json', function(req, res) {
     fs.writeFile('rentals.json', JSON.stringify(rentals, null, 4), function(err) {
       res.setHeader('Cache-Control', 'no-cache');
       res.json(rentals);
-    })
+    });
+  });
+});
+
+app.delete('rentals.json', function(req, res) {
+  fs.readFile('rentals.json', function(err, data) {
+    var rentals = JSON.parse(data);
+    for (var i in rentals) {
+      if ((rentals[i].city === req.body.city) && (rentals[i].onwer === req.body.onwer)) {
+        rentals.splice(i, 1);
+      }
+    }
+    fs.writeFile('rental.json', JSON.stringify(rentals, null, 4), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(rentals);
+    });
   });
 });
 app.listen(3000, function() {
--- public/js/main.js
+++ public/js/main.js
@@ -24,13 +24,16 @@ var RentalBox = React.createClass({
       data: rental,
       cache: false,
       success: function(data) {
-        console.log(data)
-        this.setState({ data: data })
+        console.log(data);
+        this.setState({ data: data });
       }.bind(this),
       error: function(xhr, status, err) {
         console.log(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+  onDelete: function(rental) {
+    console.log(rental);
   },
   render: function() {
     return(
@@ -38,16 +41,18 @@ var RentalBox = React.createClass({
         <h1>Rentals</h1>
         <RentalList data={this.state.data} />
         <RentalForm onRentalSubmit={this.handleRentalSubmit}/>
+        <ShowForm />
       </div>
     );
   }
 });
 
 var RentalList = React.createClass({
+
   render: function() {
     var rentalNodes = this.props.data.map(function(rental) {
       return (
-        <Rental city={rental.city}>
+        <Rental city={rental.city} owner={rental.owner} bedrooms={rental.bedrooms} onDelete={this.handleDelete}>
           <p>Owner: {rental.owner}</p>
           <p>Number of bedrooms: {rental.bedrooms}</p>
         </Rental>
@@ -64,6 +69,16 @@ var RentalList = React.createClass({
 });
 
 var Rental = React.createClass({
+  onClick: function() {
+    console.log({city: this.props.city,
+    owner: this.props.owner});
+    $.ajax({
+      
+    });
+
+
+
+  },
   render: function() {
     return (
       <div className='rental'>
@@ -71,6 +86,7 @@ var Rental = React.createClass({
           City: {this.props.city}
         </h3>
         {this.props.children}
+        <a onClick={this.onClick}>delete</a>
      </div>
     );
   }
@@ -79,6 +95,8 @@ var Rental = React.createClass({
 var RentalForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
+    $(".rentalForm").hide();
+    $(".showForm").show();
     var city = React.findDOMNode(this.refs.city).value.trim();
     var owner = React.findDOMNode(this.refs.owner).value.trim();
     var bedrooms = React.findDOMNode(this.refs.bedrooms).value.trim();
@@ -95,13 +113,25 @@ var RentalForm = React.createClass({
   render: function() {
     return (
       <form className='rentalForm' onSubmit={this.handleSubmit}>
-        <div class='form-control'>
-          <input type='text' placeholder='Enter city' ref='city' />
-          <input type='text' placeholder='Enter owner'  ref='owner' />
-          <input type='text' placeholder='Enter bedrooms' ref='bedrooms' />
+        <div className='form-group'>
+          <input type='text' className='form-control' placeholder='Enter city' ref='city' />
+          <input type='text' className='form-control' placeholder='Enter owner'  ref='owner' />
+          <input type='text' className='form-control' placeholder='Enter bedrooms' ref='bedrooms' />
         </div>
-        <input type='submit' value='Post' class='btn btn-success' />
+        <button type='submit' value='Post' className='btn btn-info'>Post</button>
       </form>
+    );
+  }
+});
+
+var ShowForm = React.createClass({
+  onClick: function() {
+    $(".rentalForm").show();
+    $(".showForm").hide();
+  },
+  render: function() {
+    return (
+      <button className='showForm btn btn-success' onClick={this.onClick}>Add Rental</button>
     );
   }
 });
